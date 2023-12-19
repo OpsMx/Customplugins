3ad5e7f21c2a8bfe3ef9fc20531a752a0503d05d
sudhakaropsmx <sudhakar@opsmx.io>
2023-12-19 10:42:17 +0530

    OP-21546: Implementation OPA validation for runtime policies OP-21546: Implementation OPA validation for Static policies

--- front50/custom-stage-front50/src/main/java/com/opsmx/plugin/stage/custom/OpaConfigProperties.java
+++ front50/custom-stage-front50/src/main/java/com/opsmx/plugin/stage/custom/OpaConfigProperties.java
@@ -1,22 +1,23 @@
-package com.opsmx.plugin.policy.runtime.config;
+package com.opsmx.plugin.stage.custom;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @ConfigurationProperties(prefix = "policy.opa")
 @EnableConfigurationProperties({OpaConfigProperties.class, OpaConfigProperties.Policy.class})
 public class OpaConfigProperties {
+
     private String url="http://opa:8181/v1/data";
     private String resultKey="deny";
     private boolean enabled=false;
     private boolean proxy=true;
     private boolean deltaVerification=false;
-    private List<Policy> policyList = new ArrayList<>();
+    private List<Policy> staticpolicies;
 
     public String getUrl() {
         return url;
@@ -58,19 +59,19 @@ public class OpaConfigProperties {
         this.deltaVerification = deltaVerification;
     }
 
-    public List<Policy> getPolicyList() {
-        return policyList;
+    public List<Policy> getStaticpolicies() {
+        return staticpolicies;
     }
 
-    public void setPolicyList(List<Policy> policyList) {
-        this.policyList = policyList;
+    public void setStaticpolicies(List<Policy> staticpolicies) {
+        this.staticpolicies = staticpolicies;
     }
 
     @Configuration
-    @ConfigurationProperties(prefix = "policy.opa.runtime")
-    public static class Policy{
-     private String name;
-     private String packageName;
+    @ConfigurationProperties(prefix = "policy.opa.staticpolicies")
+    public static class Policy {
+        private String name;
+        private String packageName;
 
         public String getName() {
             return name;
@@ -88,5 +89,4 @@ public class OpaConfigProperties {
             this.packageName = packageName;
         }
     }
-
 }

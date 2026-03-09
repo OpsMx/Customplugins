0310ce83e5ac7e5a648057753384503fed543c82
Sudhakar Guvvalapalli <sudhakar@opsmx.io>
2026-03-09 16:54:24 +0530

    OP-24522: Feature Implementation Pipeline Runtime Policies executing through ISD

---
+++ PipelineRuntimePolicyValidation/custom-policy-orca/src/main/java/com/opsmx/plugin/pipeline/runtime/policy/OpaConfigProperties.java
@@ -0,0 +1,81 @@
+package com.opsmx.plugin.pipeline.runtime.policy;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "policy.opa")
+@EnableConfigurationProperties({OpaConfigProperties.class, OpaConfigProperties.Policy.class})
+public class OpaConfigProperties {
+    private String isdUrl="http://oes-sapor:8085";
+    private String opaPolicyLocation = "/v1/data";
+    private String resultKey="deny";
+    private boolean enabled=false;
+    private List<Policy> runtime;
+
+    public String getIsdUrl() {
+        return isdUrl;
+    }
+
+    public void setIsdUrl(String isdUrl) {
+        this.isdUrl = isdUrl;
+    }
+
+    public String getOpaPolicyLocation() {
+        return opaPolicyLocation;
+    }
+
+    public void setOpaPolicyLocation(String opaPolicyLocation) {
+        this.opaPolicyLocation = opaPolicyLocation;
+    }
+
+    public String getResultKey() {
+        return resultKey;
+    }
+
+    public void setResultKey(String resultKey) {
+        this.resultKey = resultKey;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Policy> getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(List<Policy> runtime) {
+        this.runtime = runtime;
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix = "policy.opa.runtime")
+    public static class Policy{
+        private String name;
+        private String packageName;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPackageName() {
+            return packageName;
+        }
+
+        public void setPackageName(String packageName) {
+            this.packageName = packageName;
+        }
+    }
+}
